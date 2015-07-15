093b41d7f6f5ed064446e4dcba290097b5490458
Michael Williams <m.t.williams@live.com>
2015-07-15 12:04:27 -0700

    Bump version to 0.0.2-pre.

--- lib/pidgin/version.rb
+++ lib/pidgin/version.rb
@@ -1,6 +1,6 @@
 module Pidgin
   module VERSION #:nodoc:
-    MAJOR, MINOR, PATCH, PRE = [0, 0, 1, 'pre']
+    MAJOR, MINOR, PATCH, PRE = [0, 0, 2, 'pre']
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
   end
 
